d1e11ddaa435bcbf8faad6b524a41ad00d412762
fiachna <FCarter@agl.com.au>
2025-04-09 22:52:43 +1000

    add styles for text input

--- shortly-ui/src/components/atoms/text-input/index.tsx
+++ shortly-ui/src/components/atoms/text-input/index.tsx
@@ -1,18 +1,25 @@
 import { FC, Ref } from "react";
 import { BaseComponentProps } from "@/types/base-component-props";
 
+import styles from './text-input.module.scss'
+
 interface TextInputProps extends BaseComponentProps {
-	ref?: Ref<HTMLInputElement>
 	state: "default" | "error"
-	label?: string
+	placeholder?: string
+	name?: string
 	validationMessage?: string
 }
 
-const TextInput: FC<TextInputProps> = ({ ref, label, state="default", validationMessage }) => {
+const TextInput: FC<TextInputProps> = ({ placeholder, state="default", validationMessage, className, name }) => {
+	const combinedClasses = [className, styles["text-input"]].filter((c) => !!c)
+	const errorClass = state === "error" ? styles["error"] : "";
+
 	return (
-		<div>
-			<input ref={ref} />
-			<div></div>
+		<div className={combinedClasses.join(" ")}>
+			<input name={name} type="text" placeholder={placeholder} className={errorClass} />
+			<div className={styles["error-wrapper"]}>
+				<div className={styles.error}>{validationMessage}</div>
+			</div>
 		</div>
 	)
 }
