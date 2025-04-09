215a440816a9adbed76b46ae4445b0c8b09b8c33
fiachna <FCarter@agl.com.au>
2025-04-09 14:53:21 +1000

    add mobile menu toggle functionality

--- shortly-ui/src/components/atoms/nav-menu/index.tsx
+++ shortly-ui/src/components/atoms/nav-menu/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { createRef, FC } from "react";
 
 import HamburgerIcon from "@/assets/icon-hamburger-menu.svg?react"
 
@@ -6,10 +6,14 @@ import styles from "./nav-menu.module.scss"
 import LinkButton from "../link-button";
 
 const NavMenu: FC = () => {
+  const navMenuRef = createRef<HTMLElement>()
+
+  const toggleMenu = () => { navMenuRef.current?.classList.toggle(styles.visible) }
+
   return (
     <div className={styles.wrapper}>
-      <button className={styles["menu-button"]} type="button"><HamburgerIcon className="fill-stone-400 cursor-pointer" /></button>
-      <nav className={styles.navigation}>
+      <button className={styles["menu-button"]} type="button"><HamburgerIcon className="fill-stone-400 cursor-pointer" onClick={toggleMenu}/></button>
+      <nav className={styles.navigation} ref={navMenuRef}>
         <ul className={styles["navigation-list"]}>
           <li className={styles["navigation-item"]}><a className={styles.link} href="#">Features</a></li>
           <li className={styles["navigation-item"]}><a className={styles.link} href="#">Pricing</a></li>
